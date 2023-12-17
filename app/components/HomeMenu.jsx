78d95510cd6bf822a7e4824d72c0b9587db43d20
Rohan Verma <rv0181998@gmail.com>
2023-12-17 20:49:56 +0530

    first commit

---
+++ app/components/HomeMenu.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const HomeMenu = () => {
+  return (
+    <section>
+      <div className="absolute left-0 right-0 w-full justify-start">
+        <div className=" absolute left-0 -top-[70px] text-left -z-10">
+          <Image src="/sallad1.png" alt="sallad" width={109} height={189} />
+        </div>
+        <div className=" absolute -top-[100px] right-0 -z-10">
+          <Image src="/sallad2.png" alt="sallad" width={107} height={195} />
+        </div>
+      </div>
+      <div className="text-center mb-4">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">
+          Checkout
+        </h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
+    </section>
+  );
+};
+
+export default HomeMenu;
